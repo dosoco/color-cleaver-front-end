b21a99741e0bf0183e0f1691aa11dac615ded1a8
Jason Doty <jdoty@perseverenow.org>
2023-09-28 14:37:43 -0500

    color cleaver front end

---
+++ is-valid-secondary.js
@@ -0,0 +1,12 @@
+/******************
+ * YOUR CODE HERE *
+ ******************/
+
+
+
+// Our setup code here. Don't touch!
+if(typeof isValidSecondary === 'undefined') {
+  isValidSecondary = undefined;
+}
+
+module.exports = isValidSecondary;
